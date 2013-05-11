eb2516ae7154fd4398e73061843c3303350a2cb9
Nozomu Kaneko <nozom.kaneko@gmail.com>
2013-05-11 21:55:06 +0900

    add notfound view

--- pylonsprojectjp/apps/top/views.py
+++ pylonsprojectjp/apps/top/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from pyramid.view import view_config
+from pkg_resources import resource_filename
+
+from pyramid.view import view_config, notfound_view_config
+from pyramid.asset import resolve_asset_spec
+from pyramid.response import FileResponse
+
+
+@notfound_view_config()
+def notfound_view(request):
+    package, filename = resolve_asset_spec('pylonsprojectjp:static/404.html',
+                                           request.registry.__name__)
+    path = resource_filename(package, filename)
+    return FileResponse(path)
 
 
 @view_config(route_name='home', renderer="pylonsprojectjp:templates/index.jinja2")
